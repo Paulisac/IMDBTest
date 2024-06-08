33762c0c700e90fc99291842a7ca333417239e58
Paulisac <luapteg@gmail.com>
2024-06-08 15:22:24 +0530

    commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # IMDBTest
  Coding ninja test
+
+# Mini IMDB Clone
+
+## Description
+This is a mini IMDB clone app built with vanilla JavaScript and the OMDB API. It allows users to search for movies, view detailed information about selected movies, and maintain a list of favorite movies.
+
+## Features
+- Search for movies with real-time suggestions
+- View detailed information about a selected movie
+- Add movies to a list of favorites
+- Persistent storage of favorite movies using localStorage
+- Responsive design
+
+## How to Use
+1. Clone the repository: `git clone https://github.com/your-username/imdb-clone.git`
+2. Navigate to the project directory: `cd imdb-clone`
+3. Open `index.html` in your browser.
+
+## Screenshots
+- Add screenshots of your app here
+
+## Technologies Used
+- Vanilla JavaScript
+- OMDB API
+- HTML/CSS
+
+## Author
+- Paul
---
+++ script.js
@@ -0,0 +1,37 @@
+const apiKey = '2e840906';
+const searchInput = document.getElementById('search');
+const resultsDiv = document.getElementById('results');
+
+searchInput.addEventListener('input', () => {
+    const query = searchInput.value;
+    if (query.length > 2) {
+        fetch(`http://www.omdbapi.com/?s=${query}&apikey=${apiKey}`)
+            .then(response => response.json())
+            .then(data => displayResults(data.Search))
+            .catch(error => console.error('Error:', error));
+    }
+});
+
+function displayResults(movies) {
+    resultsDiv.innerHTML = '';
+    if (movies) {
+        movies.forEach(movie => {
+            const movieDiv = document.createElement('div');
+            movieDiv.classList.add('movie');
+            movieDiv.innerHTML = `
+                <span><a href="movie.html?id=${movie.imdbID}">${movie.Title} (${movie.Year})</a></span>
+                <button class="btn btn-primary" onclick="addToFavorites('${movie.imdbID}')">Favorite</button>
+            `;
+            resultsDiv.appendChild(movieDiv);
+        });
+    }
+}
+
+function addToFavorites(imdbID) {
+    let favorites = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
+    if (!favorites.includes(imdbID)) {
+        favorites.push(imdbID);
+        localStorage.setItem('favoriteMovies', JSON.stringify(favorites));
+        alert('Added to favorites!');
+    }
+}
